e0e6ab0ca26951e17c527be3b9ded90eb30be2e7
Bram van der Veen <96aa48@gmail.com>
2015-06-13 18:22:25 +0200

    Changed link to url for clarity.

--- lookup.js
+++ lookup.js
@@ -4,15 +4,15 @@ var database = require('mongoskin').db('mongodb://wallpiece/roosterio');
 var schoolid = 934;
 
 function lookup(req, res, next, search) {
-  console.log(search);
   var index = database.collection('index');
   index.find({$or : [{id : search}, {name : search}, {first_name : search}, {last_name : search}]}).toArray(function (err, database_entry) {
     if (err) console.warn(err);
 
     if (database_entry.length == 1) {
-      get(make_link(database_entry[0]), function (schedule) {
-        res.end(schedule);
-      });
+      database_entry[0].url = make_url(database_entry[0]);
+      req.match = database_entry[0];
+
+      next();
     }
     else if (database_entry.length == 0) {
       res.send('No matches were found in the database.');
@@ -23,32 +23,28 @@ function lookup(req, res, next, search) {
   });
 }
 
-function make_link(database_entry) {
-  var link = 'http://roosters5.gepro-osi.nl/roosters/rooster.php?school=' + schoolid + '&type=' + database_entry.type.charAt(0).toUpperCase() + database_entry.type.slice(1) + 'rooster';
+function make_url(database_entry) {
+  var url = 'http://roosters5.gepro-osi.nl/roosters/rooster.php?school=' + schoolid + '&type=' + database_entry.type.charAt(0).toUpperCase() + database_entry.type.slice(1) + 'rooster';
 
   switch (database_entry.type) {
     case 'leerling' :
-      link += '&afdeling=' + database_entry.studentcategory + '&leerling=' + database_entry.id;
+      url += '&afdeling=' + database_entry.studentcategory + '&leerling=' + database_entry.id;
     break;
 
     case 'docent' :
-      link += '&docenten=' + database_entry.name;
+      url += '&docenten=' + database_entry.name;
     break;
 
     case 'lokaal' :
-      link += '&lokalen=' + database_entry.name;
+      url += '&lokalen=' + database_entry.name;
     break;
 
     case 'klas' :
-      link += '&klassen=' + database_entry.name;
+      url += '&klassen=' + database_entry.name;
     break;
   }
 
-  return link;
-}
-
-function get(link, callback) {
-  console.log(link);
+  return url;
 }
 
 module.exports = lookup;
