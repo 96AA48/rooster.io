009b6e597bfef826536d70a48f023ba87914f7e8
Bram van der Veen <96aa48@gmail.com>
2015-06-14 13:40:35 +0200

    Added funcionality for using the search input bar

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,8 +1,20 @@
-// $(document).ready(function () {
-//
-//   // $('#search')
-//
-//   $('button').click(function () {
-//     document.
-//   });
-// });
+$(document).ready(function () {
+
+  $('#search').on('keyup', function (e) {
+    if (e.keyCode == 13) {
+      go();
+    }
+  });
+
+  $('button').click(function () {
+    go();
+  });
+});
+
+
+function go() {
+  var searchterm = $('#search').val().trim();
+  if (!!searchterm) {
+    document.location.pathname = '/rooster/' + searchterm;
+  }
+}
