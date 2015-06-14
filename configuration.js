3eaf81b40f6af80ecc0dd1738a26138a9896007f
Bram van der Veen <96aa48@gmail.com>
2015-06-14 23:04:49 +0200

    Made a module for loading the configuration file.

---
+++ configuration.js
@@ -0,0 +1,34 @@
+//configuration.js
+var fs = require('fs');
+
+//Function for loading and returning the settings.json file, makes one if there isn't one.
+module.exports = function () {
+  if (!fs.existsSync(__dirname + '/settings.json')) {
+    var settings = {
+      'env' : 'dev',
+      'database' : 'example.com/database',
+      'web_port' : 1024,
+      'amount_of_hours' : 7,
+      'hour_times' : [
+				'8:45 - 9:45',
+				'9:45 - 10:45',
+				'11:10 - 12:10',
+				'12:10 - 13:10',
+				'13:40 - 14:40',
+				'14:40 - 15:40',
+				'15:40 - 16:40'
+			],
+      'school_id' : 934
+    }
+    fs.writeFileSync(__dirname + '/settings.json', JSON.stringify(settings, null, 2));
+
+    return settings;
+  }
+  else {
+    return JSON.parse(fs.readFileSync(__dirname + '/settings.json'));
+  }
+}
+
+if (process.argv[2] == 'test') {
+  console.log(module.exports());
+}
