c86044882f85f87f9f2818abac7831cb97e52453
Bram van der Veen <96aa48@gmail.com>
2015-06-24 11:08:37 +0200

    Added some more deps and fixed some tabs.

--- web.js
+++ web.js
@@ -1,11 +1,13 @@
 //web.js
-var config = require('./configuration');
 var express = require('express');
 var less = require('express-less');
-var app = express();
+var body_parser = require('body-parser');
 
+var config = require('./configuration');
 var lookup = require('./lookup');
 var schedule = require('./schedule');
+
+var app = express();
 
 //Set up jade rendering engine.
 app.set('view engine', 'jade');
@@ -18,26 +20,26 @@ app.use('/js', express.static(__dirname + '/resources/js'));
 app.use('/other', express.static(__dirname + '/resources/other'));
 
 app.get('/', function (req, res) {
-    req.links = config().links;
-    res.render('homepage', req);
+  req.links = config().links;
+  console.log(req.user);
+  res.render('homepage', req);
 });
 
 app.get('/rooster/:search', function (req, res) {
-   next();
+  next();
 });
 
-
 app.get('/over', function (req, res) {
-   res.send('Hier kun je lezen over werkmanrooster.');
+  res.send('Hier kun je lezen over werkmanrooster.');
 });
 
 app.param('search', lookup);
 app.param('search', schedule);
 
 app.param('search', function (req, res) {
-   req.links = config().links;
-   req.times = config().hour_times;
-   res.render('schedule', req);
+  req.links = config().links;
+  req.times = config().hour_times;
+  res.render('schedule', req);
 });
 
 app.listen(config().web_port);
