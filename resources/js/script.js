869a95709b219cee3635e123b3cbed4476e1a145
Bram van der Veen <96aa48@gmail.com>
2016-03-22 16:08:22 +0100

    Finished advert

--- resources/js/script.js
+++ resources/js/script.js
@@ -2,4 +2,9 @@
 $(document).ready(function () {
   var day = (new Date()).getDay() - 1;
   $('#wrapper').scrollLeft($(document).width() * day);
+
+  $('#close').click(function () {
+    $('.bijles').remove();
+    $('#close').remove();
+  })
 });
